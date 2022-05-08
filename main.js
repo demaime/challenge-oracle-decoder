57aede11fda3fe9953fc4f7a02866cc9e9cf43bb
demaime <aimedemian@gmail.com>
2022-05-08 18:52:56 -0300

    encrypt and decrypt working

--- main.js
+++ main.js
@@ -3,24 +3,67 @@ const outputText = document.getElementById("output_text");
 const encryptBtn = document.getElementById("btn_encrypt");
 const decryptBtn = document.getElementById("btn_decrypt");
 
-function encrypt(x) {
-    switch(x) {
-    case 'e': return 'enter';
-    case 'i': return 'imes';
-    case 'a': return 'ai';
-    case 'o': return 'ober';
-    case 'u': return 'ufat';
-    default : return x;
+function encrypt() {
+  const enteredText = (enterTextInput.value).split("")
+  let result = "";
+
+  for (const character of enteredText) {
+    if (character === "e") {
+      result += "enter"
     }
-  }
-  
-  function codificar(s) {
-    var r = '';
-    for (const c of s) {
-        r += Cod(c);
+    else if (character === "i") {
+      result += "imes"
     }
-    return r;
+    else if (character === "a") {
+      result += "ai"
+    }
+    else if (character === "o") {
+      result += "ober"
+    }  
+    else if (character === "u") {
+      result += "ufat"
+    }
+    else  {
+      result += character
+    }
+    outputText.value = result 
   }
+}
+
+function decrypt() {
+  const textToDecrypt = (outputText.value).split("")
+  let result = "";
+
+  let i = 0;
+  while ( i < textToDecrypt.length) {
+    if (textToDecrypt[i] === "e" && textToDecrypt[i + 4] === "r") {
+      result += "e"
+      i += 5;
+    }
+    else if (textToDecrypt[i] === "i" && textToDecrypt[i + 3] === "s") {
+      result += "i"
+      i += 4;
+    }
+    else if (textToDecrypt[i] === "a" && textToDecrypt[i + 1] === "i") {
+      result += "a"
+      i += 2;
+    }
+    else if (textToDecrypt[i] === "o" && textToDecrypt[i + 3] === "r") {
+      result += "o"
+      i += 4
+    }  
+    else if (textToDecrypt[i] === "u" && textToDecrypt[i + 3] === "t") {
+      result += "u"
+      i += 4;
+    }
+    else  {
+      result += textToDecrypt[i]
+      i++;
+   }
+    console.log(result)
+    enterTextInput.value = result;
+  }
+}
 
 encryptBtn.onclick = encrypt;
-// decryptBtn.onclick = decrypt;
+decryptBtn.onclick = decrypt;
